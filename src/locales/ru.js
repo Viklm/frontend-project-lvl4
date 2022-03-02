11483bcd7fb4abf22aed17132d6bf68e691de3d7
Viktor <vik333444@yandex.ru>
2022-03-02 10:58:58 +0400

    fix text again

--- src/locales/ru.js
+++ src/locales/ru.js
@@ -57,6 +57,7 @@ export default {
     ChannelItem: {
       dropdownRemove: 'Удалить',
       dropdownRename: 'Переименовать',
+      dropdownName: 'Управление каналом',
     },
     ErrorPage: {
       title: 'Страница не найдена',
